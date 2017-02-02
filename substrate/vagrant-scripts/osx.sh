47bc54a2251ca50c929fe6a063e093cfffea45c2
Chris Roberts <code@chrisroberts.org>
2017-02-02 15:01:12 -0800

    Build substrates via vagrant and packer

---
+++ substrate/vagrant-scripts/osx.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# if the proxy is around, use it
+nc -z -w3 192.168.1.1 8123 && export http_proxy="http://192.168.1.1:8123"
+
+gem install json_pure -v '~> 1.0' --no-ri --no-rdoc
+gem install puppet -v '~> 3.0' --no-ri --no-rdoc
+TRAVIS=1 su vagrant -l -c 'ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"'
+mkdir -p /vagrant/substrate-assets
+chmod 755 /vagrant/substrate/run.sh
+
+/vagrant/substrate/run.sh /vagrant/substrate-assets
